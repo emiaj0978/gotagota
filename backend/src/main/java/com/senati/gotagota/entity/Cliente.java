73684c5c4b17e3d38fe4f0939d34e42e8159271d
emiaj0978 <vjgagoq.23@gmail.com>
2026-04-09 15:12:30 -0500

    a

--- backend/src/main/java/com/senati/gotagota/entity/Cliente.java
+++ backend/src/main/java/com/senati/gotagota/entity/Cliente.java
@@ -1,40 +1,38 @@
 package com.senati.gotagota.entity;
-
 import jakarta.persistence.*;
-
-//le dice a hibernante que esta clase esta representada
+// @Entity le dice a Hibernate que esta clase representa una tabla en la BD
 @Entity
-//indica el nombre exacto de la tabla
+// @Table indica el nombre exacto de tabla en MYSQL o MariaDB
 @Table(name = "cliente")
-
 public class Cliente {
+    // @Id marca este campo como la clave primaria de la tabla
+    // @GeneratedValue hace que el ID se genere automaticamente (AutoIncrement)
+    // @Column(name="cliente_id") indica el nombre exacto de la columna en mysql
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cliente_id")
-    private long Id;
-
-
+    private Long id;
+    // nullable=false significa que este campo no puede estar vacio en la BD
     @Column(nullable = false)
     private String nombre;
-
-
     @Column(nullable = false)
     private String apellido;
-
-
+    //unique=true significa que no puede haber dos clientes con el mismo DNI
+    //length=8 significa limita el campo a 8 caracteres
     @Column(nullable = false,unique = true,length = 8)
     private String dni;
-
-    //sin anotaciones (@) extra: columna normal, puede ser nula
+    //Sin anotaciones ( @ ) extra: columna normal, puede ser nula
     private String telefono;
     private String direccion;
 
-    public long getId() {
-        return Id;
+    //GETTER a SETTERS
+
+    public Long getId() {
+        return id;
     }
 
-    public void setId(long id) {
-        Id = id;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNombre() {
@@ -61,19 +59,19 @@ public class Cliente {
         this.dni = dni;
     }
 
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
     public String getDireccion() {
         return direccion;
     }
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 }
--- backend/src/main/java/com/senati/gotagota/service/ClienteService.java
+++ backend/src/main/java/com/senati/gotagota/service/ClienteService.java
@@ -4,17 +4,18 @@ import com.senati.gotagota.entity.Cliente;
 import com.senati.gotagota.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
-//importamos la anotacion @service
-//esta es la capa de a logica de nogocio, aqui van las validaciones, calculos,etc.
+//Importamos la anotacion @service
+//Esto es la capa de la logica de negocio, aqui van las validaciones, calculos, etc.
 @Service
 public class ClienteService {
-
+    //Inyectamos el repositorio para poder acceder a la base de datos
     private final ClienteRepository clienteRepository;
 
-    public ClienteService(ClienteRepository clienteRepository){
+    //Contructor: Spring inyecta automaticamente el repositorio(las dependencias)
+    public ClienteService(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
     }
+    //Retorna o recive la lista de todos los clientes
     public List<Cliente> listarTodos(){
         return clienteRepository.findAll();
     }
