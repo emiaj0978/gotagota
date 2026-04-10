bf688ab7e73fa11137462fe17623256290fc2f1b
emiaj0978 <vjgagoq.23@gmail.com>
2026-04-10 15:29:22 -0500

    a

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,9 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:8080/api/clientes')
         .then(response => response.json())
         .then(data => {
-            console.log('Datos:', data);
-            const elemento = document.getElementById("tabla-cliente")
-            elemento.innerHTML = JSON.stringify(data);
-            console.log(elemento)
+
+            //console.log('Datos:', data);
+            const elemento = document.getElementById("table-cliente")
+
+            for (let i = 0; i < data.length; i++) {
+                let cliente = data[i];
+                let fila = `
+                <tr>
+                        <td>${cliente.id}</td>
+                        <td>${cliente.nombre}</td>
+                        <td>${cliente.apellido}</td>
+                        <td>${cliente.dni}</td>
+                        <td>${cliente.telefono}</td>
+                        <td>${cliente.direccion}</td>
+                </tr>
+                        `
+                elemento.innerHTML += fila;
+                //console.log(cliente)
+
+            }
+            //elemento.innerHTML = JSON.stringify(data);
+            //console.log(elemento)
         })
 });
